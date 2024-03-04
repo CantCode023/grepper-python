d79aaa4979cbad93be865e3d187db156cbfbb912
CantCode023 <cantcode023@gmail.com>
2024-03-04 16:33:13 +0800

    deleted unnecessary stuffs and added __all__

--- grepper_python/__init__.py
+++ grepper_python/__init__.py
@@ -10,18 +10,15 @@ __license__ = "MIT"
 __copyright__ = "Copyright 2010-2023 Grepper, Inc."
 __version__ = "0.0.1a"
 
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+from grepper_python.answer import GrepperAnswer
+from grepper_python.main import Grepper
+from grepper_python.exceptions import BadRequest
+from grepper_python.exceptions import Unauthorized
+from grepper_python.exceptions import Forbidden
+from grepper_python.exceptions import NotFound
+from grepper_python.exceptions import MethodNotAllowed
+from grepper_python.exceptions import TooManyRequests
+from grepper_python.exceptions import InternalServerError
+from grepper_python.exceptions import ServiceUnavailable
 
-import logging
-from typing import NamedTuple, Literal
-
-from .answer import GrepperAnswer
-from .main import Grepper
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-del (
-    logging,
-    NamedTuple,
-    Literal,
-)
+__all__ = ["GrepperAnswer", "Grepper", "BadRequest", "Unauthorized", "Forbidden", "NotFound", "MethodNotAllowed", "TooManyRequests", "InternalServerError", "ServiceUnavailable"]
