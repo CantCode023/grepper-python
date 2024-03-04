6321d98ec09b2cdae142ee661ef0aeaf2295be8a
CantCode023 <cantcode023@gmail.com>
2024-03-04 16:34:24 +0800

    added exception base class for raising exceptions

--- grepper_python/exceptions.py
+++ grepper_python/exceptions.py
@@ -6,40 +6,40 @@ This module contains the set of grepper-python's exceptions.
 
 
 # 400
-class BadRequest:
+class BadRequest(Exception):
     """Your request is invalid."""
 
 
 # 401
-class Unauthorized:
+class Unauthorized(Exception):
     """Your API key is wrong."""
 
 
 # 403
-class Forbidden:
+class Forbidden(Exception):
     """You do not have access to the requested resource."""
 
 
 # 404
-class NotFound:
+class NotFound(Exception):
     """The specified enpoint could not be found."""
 
 
 # 405
-class MethodNotAllowed:
+class MethodNotAllowed(Exception):
     """You tried to access an enpoint with an invalid method."""
 
 
 # 429
-class TooManyRequests:
+class TooManyRequests(Exception):
     """You're making too many requests! Slow down!"""
 
 
 # 500
-class InternalServerError:
+class InternalServerError(Exception):
     """We had a problem with our server. Try again later."""
 
 
 # 503
-class ServiceUnavailable:
+class ServiceUnavailable(Exception):
     """We're temporarily offline for maintenance. Please try again later."""
