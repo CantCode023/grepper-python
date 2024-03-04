94122208e82db213b9545458745c97061aa6a118
CantCode023 <cantcode023@gmail.com>
2024-03-04 18:32:09 +0800

    renamed DocDefaultException to GrepperDefaultException

--- grepper_python/exceptions.py
+++ grepper_python/exceptions.py
@@ -4,45 +4,45 @@ grepper-python.exceptions
 This module contains the set of grepper-python's exceptions.
 """
 
-class DocDefaultException(Exception):
+class GrepperDefaultException(Exception):
     def __init__(self, msg=None, *args, **kwargs):
         super().__init__(msg or self.__doc__, *args, **kwargs)
 
 # 400
-class BadRequest(DocDefaultException):
+class BadRequest(GrepperDefaultException):
     """Your request is invalid."""
 
 
 # 401
-class Unauthorized(DocDefaultException):
+class Unauthorized(GrepperDefaultException):
     """Your API key is wrong."""
 
 
 # 403
-class Forbidden(DocDefaultException):
+class Forbidden(GrepperDefaultException):
     """You do not have access to the requested resource."""
 
 
 # 404
-class NotFound(DocDefaultException):
+class NotFound(GrepperDefaultException):
     """The specified enpoint could not be found."""
 
 
 # 405
-class MethodNotAllowed(DocDefaultException):
+class MethodNotAllowed(GrepperDefaultException):
     """You tried to access an enpoint with an invalid method."""
 
 
 # 429
-class TooManyRequests(DocDefaultException):
+class TooManyRequests(GrepperDefaultException):
     """You're making too many requests! Slow down!"""
 
 
 # 500
-class InternalServerError(DocDefaultException):
+class InternalServerError(GrepperDefaultException):
     """We had a problem with our server. Try again later."""
 
 
 # 503
-class ServiceUnavailable(DocDefaultException):
+class ServiceUnavailable(GrepperDefaultException):
     """We're temporarily offline for maintenance. Please try again later."""
