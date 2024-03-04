48e4af4e5ac68a711fc8a347534a2fa76c624032
CantCode023 <cantcode023@gmail.com>
2024-03-04 17:10:38 +0800

    wrote exceptions in a tuple

--- grepper_python/__init__.py
+++ grepper_python/__init__.py
@@ -12,13 +12,15 @@ __version__ = "0.0.1a"
 
 from grepper_python.answer import GrepperAnswer
 from grepper_python.main import Grepper
-from grepper_python.exceptions import BadRequest
-from grepper_python.exceptions import Unauthorized
-from grepper_python.exceptions import Forbidden
-from grepper_python.exceptions import NotFound
-from grepper_python.exceptions import MethodNotAllowed
-from grepper_python.exceptions import TooManyRequests
-from grepper_python.exceptions import InternalServerError
-from grepper_python.exceptions import ServiceUnavailable
+from grepper_python.exceptions import (
+    BadRequest,
+    Unauthorized,
+    Forbidden,
+    NotFound,
+    MethodNotAllowed,
+    TooManyRequests,
+    InternalServerError,
+    ServiceUnavailable
+)
 
 __all__ = ["GrepperAnswer", "Grepper", "BadRequest", "Unauthorized", "Forbidden", "NotFound", "MethodNotAllowed", "TooManyRequests", "InternalServerError", "ServiceUnavailable"]
