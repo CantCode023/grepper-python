540d09e61a3f041520480e93ea8cfc91da64c938
CantCode023 <cantcode023@gmail.com>
2024-03-04 16:43:29 +0800

    made the error message more customizable

--- grepper_python/exceptions.py
+++ grepper_python/exceptions.py
@@ -4,42 +4,45 @@ grepper-python.exceptions
 This module contains the set of grepper-python's exceptions.
 """
 
+class DocDefaultException(Exception):
+    def __init__(self, msg:str=None, *args, **kwargs):
+        super().__init__(msg or self.__doc__, *args, **kwargs)
 
 # 400
-class BadRequest(Exception):
+class BadRequest(DocDefaultException):
     """Your request is invalid."""
 
 
 # 401
-class Unauthorized(Exception):
+class Unauthorized(DocDefaultException):
     """Your API key is wrong."""
 
 
 # 403
-class Forbidden(Exception):
+class Forbidden(DocDefaultException):
     """You do not have access to the requested resource."""
 
 
 # 404
-class NotFound(Exception):
+class NotFound(DocDefaultException):
     """The specified enpoint could not be found."""
 
 
 # 405
-class MethodNotAllowed(Exception):
+class MethodNotAllowed(DocDefaultException):
     """You tried to access an enpoint with an invalid method."""
 
 
 # 429
-class TooManyRequests(Exception):
+class TooManyRequests(DocDefaultException):
     """You're making too many requests! Slow down!"""
 
 
 # 500
-class InternalServerError(Exception):
+class InternalServerError(DocDefaultException):
     """We had a problem with our server. Try again later."""
 
 
 # 503
-class ServiceUnavailable(Exception):
+class ServiceUnavailable(DocDefaultException):
     """We're temporarily offline for maintenance. Please try again later."""
